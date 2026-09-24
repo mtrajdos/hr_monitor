240102b40fdff732860e77e2fcf9c438b7225e9b
Michal Trajdos <michal.trajdos95@gmail.com>
2026-09-24 19:46:20 +0200

    Roadmap re-do

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,8 @@ CREATE TABLE IF NOT EXISTS hr_readings (
     sample_id INTEGER PRIMARY KEY AUTOINCREMENT,
     session_id TEXT NOT NULL,
     timestamp TEXT NOT NULL,
-    bpm INTEGER NOT NULL
+    bpm INTEGER NOT NULL,
+    acc_x REAL NOT NULL,
+    acc_y REAL NOT NULL,
+    acc_z REAL NOT NULL
 );
